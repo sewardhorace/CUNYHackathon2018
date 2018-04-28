2f512daace8ecce753439e3cd411719e5129e139
sewardhorace <maxjwhite22@gmail.com>
2018-04-28 18:08:22 -0400

    navigation stack demo

--- RealTalk/App.js
+++ RealTalk/App.js
@@ -5,44 +5,49 @@ import {
 } from 'react-navigation';
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
   render() {
-    const { navigate } = this.props.navigation;
     return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() =>
-          navigate('Profile', { name: 'Jane' })
-        }
-      />
-    );
-  }
-}
-
-class ProfileScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Butthead',
-  };
-  render() {
-    const { navigate } = this.props.navigation;
-    state = {
-      counter: 1,
-    };
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! {this.state.counter}</Text>
-        <Button onPress={()=> this.setState({counter: this.state.counter+= 1})} title="click me"/>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
       </View>
     );
   }
 }
 
-export const App = StackNavigator({
-  Home: { screen: HomeScreen },
-  Profile: { screen: ProfileScreen },
-});
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
 
 
 // export default class App extends React.Component {
