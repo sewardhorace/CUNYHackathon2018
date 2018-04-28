6c5bf3745f10202fd7b2a2a3b22fb9b37ffb1c38
sewardhorace <maxjwhite22@gmail.com>
2018-04-28 17:33:56 -0400

    package.json

--- RealTalk/App.js
+++ RealTalk/App.js
@@ -1,15 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile', { name: 'Jane' })
+        }
+      />
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Butthead',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    state = {
+      counter: 1,
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app! {this.state.counter}</Text>
+        <Button onPress={()=> this.setState({counter: this.state.counter+= 1})} title="click me"/>
       </View>
     );
   }
 }
+
+export const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+
+// export default class App extends React.Component {
+//   state = {
+//     counter: 1,
+//   }
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app! {this.state.counter}</Text>
+//         <Button onPress={()=> this.setState({counter: this.state.counter+= 1})} title="click me"/>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
